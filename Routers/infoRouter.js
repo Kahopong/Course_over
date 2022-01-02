1bf2c1716769ff5568d7534ebb34759de828d542
fankilky <91205739+fankilky@users.noreply.github.com>
2022-01-02 20:31:20 +0800

    fankii

--- Routers/infoRouter.js
+++ Routers/infoRouter.js
@@ -1,79 +1,78 @@
 class InfoRouter {
-    constructor(infoService, express) {
-        this.infoService = infoService;
-        this.express = express
-    }
+  constructor(infoService, express) {
+    this.infoService = infoService;
+    this.express = express;
+  }
 
-    router() {
-        let router = this.express.Router();
-        router.get('/users', this.getUsers.bind(this))
-        router.get('/shop', this.getShop.bind(this))
-        router.put('/users', this.putUsers.bind(this))
-        router.put('/shop', this.putShop.bind(this))
+  router() {
+    let router = this.express.Router();
+    router.get("/users", this.getUsers.bind(this));
+    router.get("/shop", this.getShop.bind(this));
+    router.put("/users", this.putUsers.bind(this));
+    router.put("/shop", this.putShop.bind(this));
 
-        return router;
-    }
+    return router;
+  }
 
-    // GET Users Info
-    // ==================================
-    getUsers(req, res) {
-        return (this.infoService.listUser(2)
-            .then((data) => {
-                console.log('hello');
-                // console.log(req.session);
-                res.json(data);
-            })
-            .catch((err) => {
-                res.status(500)
-                return res.json(err)
-            }));
-    }
+  // GET Users Info
+  // ==================================
+  getUsers(req, res) {
+    return this.infoService
+      .listUser(2)
+      .then((data) => {
+        console.log("hello");
+        // console.log(req.session);
+        res.json(data);
+      })
+      .catch((err) => {
+        res.status(500);
+        return res.json(err);
+      });
+  }
 
-    // GET Shop Info
-    // ==================================
-    getShop(req, res) {
-        return (this.infoService.listShop(2)
-            .then((data) => {
-                res.json(data)
-            })
-            .catch((err) => {
-                res.status(500)
-                return res.json(err)
-            }));
-    }
+  // GET Shop Info
+  // ==================================
+  getShop(req, res) {
+    return this.infoService
+      .listShop(2)
+      .then((data) => {
+        res.json(data);
+      })
+      .catch((err) => {
+        res.status(500);
+        return res.json(err);
+      });
+  }
 
+  // Edit Users Info
+  // ==================================
+  putUsers(req, res) {
+    return this.infoService
+      .editUser(2, req.body.edit)
+      .then(() => this.infoService.listUser(2))
+      .then((data) => {
+        res.json(data);
+      })
+      .catch((err) => {
+        res.status(500);
+        return res.json(err);
+      });
+  }
 
-    // Edit Users Info
-    // ==================================
-    putUsers(req, res) {
-        return (this.infoService
-            .editUser(3, req.body.edit)
-            .then(() => this.infoService.listUser(3))
-            .then((data) => {
-                res.json(data)
-            })
-            .catch((err) => {
-                res.status(500)
-                return res.json(err)
-            })
-        )
-    }
-
-    // Edit Shop Info
-    // ==================================
-    putShop(req, res) {
-        return (this.infoService
-            .editShop(3, req.body.edit)
-            .then(() => this.infoService.listShop(3))
-            .then((data) => {
-                res.json(data)
-            })
-            .catch((err) => {
-                res.status(500)
-                return res.json(err)
-            })
-        );
-    }
+  // Edit Shop Info
+  // ==================================
+  putShop(req, res) {
+    return this.infoService
+      .editShop(2, req.body.edit)
+      .then(() => this.infoService.listShop(2))
+      .then((data) => {
+        res.json(data);
+      })
+      .catch((err) => {
+        res.status(500);
+        return res.json(err);
+      });
+  }
 }
 
 module.exports = InfoRouter;
